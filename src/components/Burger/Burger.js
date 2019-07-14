c5d43322d00e77dd25eb6807098a89cde434cd5d
Dima-f <dafw@live.ru>
2019-07-15 00:23:07 +0300

    start burger ingradients dynamically

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Burger.module.css';
+import BurgerIngradient from './BurgerIngradient/BurgerIngradient';
+
+const burger = (props) => {
+    let transformedIngradients = Object.keys(props.ingradients)
+        .map(igKey => {
+            return [...Array(props.ingradients[igKey])].map((_,i)=><BurgerIngradient key={igKey + i} type={igKey} />
+            );
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngradients.length === 0) {
+        transformedIngradients = <p>Please start adding ingradients!</p>
+    }
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngradient type="bread-top" />
+            {transformedIngradients}
+            <BurgerIngradient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
