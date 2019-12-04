7138491e35b3cf70e9a746f1a2b3ee046881a332
Dima-f <dafw@live.ru>
2019-12-04 23:43:09 +0200

    env in redux store

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import styles from './Auth.module.css';
 import { Redirect } from 'react-router-dom';
 import * as actions from '../../store/actions/index';
+import { checkValidity } from '../../shared/utility';
 
 export class Auth extends Component {
     state = {
@@ -43,6 +44,7 @@ export class Auth extends Component {
     }
 
     componentDidMount() {
+        console.log(process);
         if(!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
             this.props.setAuthRedirectPath('/');
         }
@@ -56,44 +58,13 @@ export class Auth extends Component {
         });
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (e, controlName) => {
         const updatedControls = {
             ...this.state.controls,
             [controlName]: {
                 ...this.state.controls[controlName],
                 value: e.target.value,
-                valid: this.checkValidity(e.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(e.target.value, this.state.controls[controlName].validation),
                 touched: true
             }
         }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import axios from '../../../axios-orders';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { checkValidity } from '../../../shared/utility';
 
 export class ContactData extends Component {
     state = {
@@ -93,36 +94,6 @@ export class ContactData extends Component {
         },
         formIsValid: false
     };
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
-    }
 
     inputChangedHandler = (e, inputIdentifier) => {
         const updatedOrderForm = {
@@ -133,7 +104,7 @@ export class ContactData extends Component {
         };
 
         updatedOrderElement.value = e.target.value;
-        updatedOrderElement.valid = this.checkValidity(updatedOrderElement.value, updatedOrderElement.validation);
+        updatedOrderElement.valid = checkValidity(updatedOrderElement.value, updatedOrderElement.validation);
         updatedOrderElement.touched = true;
 
         let formIsValid = true;
